282353999989247fbefe20432a0bda656cdf3e40
Matheus Coutinho <lammerlopes@hotmail.com>
2024-09-03 14:23:28 -0300

    Curso JS TypeScript

---
+++ src/A0030-associacao/A0030-associacao.ts
@@ -0,0 +1,63 @@
+// class association -> Quando uma class se associa-se com outra
+// porém uma não é depedente da outra
+export class Escritor {
+  constructor(
+    private _nome: string,
+    private _ferramenta: Ferramenta | null = null,
+  ) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this.ferramenta === null) {
+      console.log(`Não posso escrever sem ferramenta.`);
+      return;
+    }
+    this.ferramenta.escrever();
+  }
+}
+
+export abstract class Ferramenta {
+  constructor(private _nome: string) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  abstract escrever(): void;
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome}, está escrevendo...`);
+  }
+}
+
+export class MaquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome}, está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Luiz');
+const ferramentaCaneta = new Caneta('Caneta');
+const ferramentaMaquina = new MaquinaEscrever('Máquina de escrever');
+
+// console.log(escritor.nome);
+// console.log(ferramentaCaneta.nome);
+// console.log(ferramentaMaquina.nome);
+
+escritor.ferramenta = ferramentaCaneta;
+escritor.ferramenta = ferramentaMaquina;
+escritor.ferramenta = null;
+escritor.escrever();
