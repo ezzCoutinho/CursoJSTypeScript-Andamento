c5815882b64f6cde085c5c1e0409c07ccfc8faeb
Matheus Coutinho <lammerlopes@hotmail.com>
2024-09-09 11:36:27 -0300

    Curso JS TypeScript

---
+++ src/A0043-generics/A0043-generics.ts
@@ -0,0 +1,33 @@
+// <generics> -> Quando não sabemos oq iremos receber, mas será usado
+// como inferimento de tipo.
+// <QUALQUERCOISA> -> recebendo qualquer coisa dentro, é apenas um tipo
+// "desconhecido", pode ser usado em qualquer local.
+
+type FilterCallback<U> = (
+  value: U,
+  index?: number,
+  array?: U[],
+) => boolean;
+
+export function myFilter<T>(
+  array: T[],
+  callbackfn: FilterCallback<T>,
+): T[] {
+  const novoArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      novoArray.push(array[i]);
+    }
+  }
+
+  return novoArray;
+}
+
+const arrayNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayString = ['Hello', 'World'];
+
+const arrayFiltradoOriginal = arrayNumbers.filter((value) => value < 5);
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = myFilter(arrayNumbers, (value) => value < 5);
+console.log(arrayFiltrado);
