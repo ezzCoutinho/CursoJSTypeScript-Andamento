b87450ec3eb79fdaf2db787506e5828dae32e669
Matheus Coutinho <lammerlopes@hotmail.com>
2024-09-09 14:12:11 -0300

    Curso JS TypeScript

---
+++ src/A0046-generics-constraints/A0046-generics-constraints.ts
@@ -0,0 +1,19 @@
+// Constraints -> quando um tipo pode ser qualquer coisa, e precisa
+// no "máximo" algo que ligue a ela, a outro tipo.
+// <O, K extends keyof O>
+
+type ObterChaveFn = <O, K extends keyof O>(objeto: O, chave: K) => O[K];
+
+const obterChave: ObterChaveFn = (objeto, chave) => objeto[chave];
+
+const animal = {
+  cor: 'Rosa',
+  vacina: ['Vacina 1', 'Vacina 2'],
+  idade: 8,
+};
+
+const vacina = obterChave(animal, 'vacina');
+const cor = obterChave(animal, 'cor');
+const idade = obterChave(animal, 'idade');
+
+console.log(vacina, cor, idade);
